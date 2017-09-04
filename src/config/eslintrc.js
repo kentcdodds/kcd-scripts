40e8d34fb04c8c8df115f250e66842411e2618d4
Kent C. Dodds <kent@doddsfamily.us>
2017-09-03 23:59:14 -0600

    fix config again

--- src/config/eslintrc.js
+++ src/config/eslintrc.js
@@ -6,33 +6,16 @@ module.exports = {
     'kentcdodds/jest',
     ifAnyDep('react', 'kentcdodds/jsx-a11y'),
     ifAnyDep('react', 'kentcdodds/react'),
-    'prettier',
     'kentcdodds/prettier',
+    'prettier',
   ].filter(Boolean),
-  rules: Object.assign(
-    {
-      // stuff I haven't gotten around to updating in my config
-      'no-unused-vars': [
-        'error',
-        {argsIgnorePattern: '^_', varsIgnorePattern: '^ignored'},
-      ],
-      'func-style': 'off',
-      'no-process-exit': 'off',
-
-      // prettier does this for us
-    },
-    ifAnyDep(
-      'prettier',
-      {
-        'max-len': 'off',
-        semi: 'off',
-        quotes: 'off',
-        'comma-dangle': 'off',
-        'no-console': 'off',
-        indent: 'off',
-        'babel/object-curly-spacing': 'off',
-      },
-      {},
-    ),
-  ),
+  rules: {
+    // stuff I haven't gotten around to updating in my config
+    'no-unused-vars': [
+      'error',
+      {argsIgnorePattern: '^_', varsIgnorePattern: '^ignored'},
+    ],
+    'func-style': 'off',
+    'no-process-exit': 'off',
+  },
 }

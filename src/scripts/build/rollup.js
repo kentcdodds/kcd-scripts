0dce5bce085f44a8133773000ba66dc9b8188734
Kent C. Dodds <kent@doddsfamily.us>
2017-09-08 12:49:01 -0600

    feat(build): support --watch and --environment flags

--- src/scripts/build/rollup.js
+++ src/scripts/build/rollup.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const spawn = require('cross-spawn')
 const rimraf = require('rimraf')
+const yargsParser = require('yargs-parser')
 const {
   hasFile,
   resolveBin,
@@ -12,6 +13,7 @@ const crossEnv = resolveBin('cross-env')
 const rollup = resolveBin('rollup')
 const args = process.argv.slice(2)
 const here = p => path.join(__dirname, p)
+const parsedArgs = yargsParser(args)
 
 const useBuiltinConfig =
   !args.includes('--config') && !hasFile('rollup.config.js')
@@ -19,8 +21,17 @@ const config = useBuiltinConfig
   ? `--config ${here('../../config/rollup.config.js')}`
   : args.includes('--config') ? '' : '--config' // --config will pick up the rollup.config.js file
 
+const environment = parsedArgs.environment
+  ? `--environment ${parsedArgs.environment}`
+  : ''
+const watch = parsedArgs.watch ? '--watch' : ''
+
+const defaultEnv = 'BUILD_ROLLUP=true'
+
 const getCommand = env =>
-  `${crossEnv} BUILD_ROLLUP=true ${env} ${rollup} ${config}`
+  [crossEnv, defaultEnv, env, rollup, config, environment, watch]
+    .filter(Boolean)
+    .join(' ')
 
 const scripts = args.includes('--p-react')
   ? getPReactScripts()

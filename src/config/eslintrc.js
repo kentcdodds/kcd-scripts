0e06077ea6e326f62d50db0b150f43143ac3a1b8
Kent C. Dodds <kent@doddsfamily.us>
2017-09-19 22:58:16 -0600

    fix: update eslint config

--- src/config/eslintrc.js
+++ src/config/eslintrc.js
@@ -6,17 +6,6 @@ module.exports = {
     require.resolve('eslint-config-kentcdodds/jest'),
     ifAnyDep('react', require.resolve('eslint-config-kentcdodds/jsx-a11y')),
     ifAnyDep('react', require.resolve('eslint-config-kentcdodds/react')),
-    require.resolve('eslint-config-kentcdodds/prettier'),
-    require.resolve('eslint-config-prettier'),
   ].filter(Boolean),
-  rules: {
-    // stuff I haven't gotten around to updating in my config
-    'no-unused-vars': [
-      'error',
-      {argsIgnorePattern: '^_', varsIgnorePattern: '^ignored'},
-    ],
-    'func-style': 'off',
-    'no-process-exit': 'off',
-    'comma-dangle': 'off',
-  },
+  rules: {},
 }

39513ae8db8c0fc2d80b0a43aeb5a4ee6e5648fe
Kent C. Dodds <kent@doddsfamily.us>
2017-09-23 21:41:33 -0600

    fix(format): default should not be quoted I guess

--- src/scripts/format.js
+++ src/scripts/format.js
@@ -30,9 +30,7 @@ const write = args.includes('--no-write') ? [] : ['--write']
 // This way the prettierignore will be applied
 const relativeArgs = args.map(a => a.replace(`${process.cwd()}/`, ''))
 
-const filesToApply = parsedArgs._.length
-  ? []
-  : ['"**/*.+(js|json|less|css|ts)"']
+const filesToApply = parsedArgs._.length ? [] : ['**/*.+(js|json|less|css|ts)']
 
 const result = spawn.sync(
   resolveBin('prettier'),

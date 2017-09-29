9f109829669748537a36024bc36196a97b2961b5
Kent C. Dodds <kent@doddsfamily.us>
2017-09-28 23:59:50 -0600

    feat(help): add help

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,1 +1,95 @@
-test('works', () => {})
+import path from 'path'
+import cases from 'jest-in-case'
+
+// this removes the quotes around strings...
+const projectRoot = path.join(__dirname, '../../').replace(/\\/g, '/')
+
+expect.addSnapshotSerializer({
+  print(val) {
+    return val.split(projectRoot).join('<PROJECT_ROOT>/')
+  },
+  test(val) {
+    return typeof val === 'string'
+  },
+})
+
+cases(
+  'format',
+  ({
+    snapshotLog = false,
+    throws = false,
+    setup = () => () => {},
+    signal = false,
+    args = [],
+  }) => {
+    // beforeEach
+    const {sync: crossSpawnSyncMock} = require('cross-spawn')
+    const originalExit = process.exit
+    const originalArgv = process.argv
+    const originalLog = console.log
+    process.exit = jest.fn()
+    console.log = jest.fn()
+    const teardown = setup()
+
+    try {
+      // tests
+      process.argv = ['node', '../', ...args]
+      crossSpawnSyncMock.mockClear()
+      if (signal) {
+        crossSpawnSyncMock.mockReturnValueOnce({result: 1, signal})
+      }
+      require('../')
+      if (snapshotLog) {
+        expect(console.log.mock.calls).toMatchSnapshot()
+      } else if (signal) {
+        expect(process.exit).toHaveBeenCalledTimes(1)
+        expect(process.exit).toHaveBeenCalledWith(1)
+        expect(console.log.mock.calls).toMatchSnapshot()
+      } else {
+        expect(crossSpawnSyncMock).toHaveBeenCalledTimes(1)
+        const [firstCall] = crossSpawnSyncMock.mock.calls
+        const [script, calledArgs] = firstCall
+        expect([script, ...calledArgs].join(' ')).toMatchSnapshot()
+      }
+    } catch (error) {
+      if (throws) {
+        expect(error.message).toMatchSnapshot()
+      } else {
+        throw error
+      }
+    } finally {
+      teardown()
+      // afterEach
+      process.exit = originalExit
+      process.argv = originalArgv
+      console.log = originalLog
+      jest.resetModules()
+    }
+  },
+  {
+    'calls node with the script path and args': {
+      args: ['test', '--no-watch'],
+    },
+    'throws unknown script': {
+      args: ['unknown-script'],
+      throws: true,
+    },
+    'logs help with no args': {
+      snapshotLog: true,
+    },
+    'logs for SIGKILL signal': {
+      args: ['lint'],
+      signal: 'SIGKILL',
+    },
+    'logs for SIGTERM signal': {
+      args: ['build'],
+      signal: 'SIGTERM',
+    },
+    'does not log for other signals': {
+      args: ['test'],
+      signal: 'SIGBREAK',
+    },
+  },
+)
+
+/* eslint complexity:0 */
--- src/index.js
+++ src/index.js
@@ -1,57 +1,94 @@
 #!/usr/bin/env node
 const path = require('path')
 const spawn = require('cross-spawn')
+const glob = require('glob')
 
-function attemptResolve(...args) {
-  try {
-    return require.resolve(...args)
-  } catch (error) {
-    return null
+const [executor, ignoredBin, script, ...args] = process.argv
+if (script) {
+  spawnScript()
+} else {
+  const scriptsPath = path.join(__dirname, 'scripts/')
+  const scriptsAvailable = glob.sync(path.join(__dirname, 'scripts', '*'))
+  const scriptsAvailableMessage = scriptsAvailable
+    .map(s =>
+      s
+        .replace(scriptsPath, '')
+        .replace(/__tests__/, '')
+        .replace(/\.js$/, ''),
+    )
+    .filter(Boolean)
+    .join('\n  ')
+    .trim()
+  const fullMessage = `
+Usage: ${ignoredBin} [script] [--flags]
+
+Available Scripts:
+  ${scriptsAvailableMessage}
+
+Options:
+  All options depend on the script. Docs will be improved eventually, but for most scripts you can assume that the args you pass will be forwarded to the respective tool that's being run under the hood.
+
+May the force be with you.
+  `.trim()
+  console.log(`\n${fullMessage}\n`)
+}
+
+function getEnv() {
+  // this is required to address an issue in cross-spawn
+  // https://github.com/kentcdodds/kcd-scripts/issues/4
+  return Object.keys(process.env)
+    .filter(key => process.env[key] !== undefined)
+    .reduce(
+      (envCopy, key) => {
+        envCopy[key] = process.env[key]
+        return envCopy
+      },
+      {
+        [`SCRIPTS_${script.toUpperCase()}`]: true,
+      },
+    )
+}
+
+function spawnScript() {
+  const relativeScriptPath = path.join(__dirname, './scripts', script)
+  const scriptPath = attemptResolve(relativeScriptPath)
+
+  if (!scriptPath) {
+    throw new Error(`Unknown script "${script}".`)
+  }
+  const result = spawn.sync(executor, [scriptPath, ...args], {
+    stdio: 'inherit',
+    env: getEnv(),
+  })
+
+  if (result.signal) {
+    handleSignal(result)
+  } else {
+    process.exit(result.status)
   }
 }
 
-const [executor, ignoredBin, script, ...args] = process.argv
-const relativeScriptPath = path.join(__dirname, './scripts', script)
-const scriptPath = attemptResolve(relativeScriptPath)
-
-if (!scriptPath) {
-  console.log(`Unknown script "${script}".`)
-  console.log('Perhaps you need to update kcd-scripts?')
-}
-
-// this is required to address an issue in cross-spawn
-// https://github.com/kentcdodds/kcd-scripts/issues/4
-const env = Object.keys(process.env)
-  .filter(key => process.env[key] !== undefined)
-  .reduce(
-    (envCopy, key) => {
-      envCopy[key] = process.env[key]
-      return envCopy
-    },
-    {
-      [`SCRIPTS_${script.toUpperCase()}`]: true,
-    },
-  )
-
-const result = spawn.sync(executor, [scriptPath, ...args], {
-  stdio: 'inherit',
-  env,
-})
-
-if (result.signal) {
+function handleSignal(result) {
   if (result.signal === 'SIGKILL') {
     console.log(
-      'The build failed because the process exited too early. ' +
+      `The script "${script}" failed because the process exited too early. ` +
         'This probably means the system ran out of memory or someone called ' +
         '`kill -9` on the process.',
     )
   } else if (result.signal === 'SIGTERM') {
     console.log(
-      'The build failed because the process exited too early. ' +
+      `The script "${script}" failed because the process exited too early. ` +
         'Someone might have called `kill` or `killall`, or the system could ' +
         'be shutting down.',
     )
   }
   process.exit(1)
 }
-process.exit(result.status)
+
+function attemptResolve(...resolveArgs) {
+  try {
+    return require.resolve(...resolveArgs)
+  } catch (error) {
+    return null
+  }
+}

afe5d0bfe64f534e1f23e5d8523ffa1e282258d7
Suhas Karanth <sudo.suhas@gmail.com>
2017-11-10 08:14:33 +0530

    test: Fix failing tests on Windows
    - [src\__tests__\index.js] format › calls node with the script path and args
    - [src\scripts\__tests__\validate.js] validate › allows you to specify your
      own npm scripts

--- src/scripts/__tests__/validate.js
+++ src/scripts/__tests__/validate.js
@@ -75,10 +75,14 @@ function setupWithScripts(scripts = ['test', 'lint', 'build', 'flow']) {
 
 function setupWithArgs(args = []) {
   return function setup() {
+    const utils = require('../../utils')
+    const originalResolveBin = utils.resolveBin
+    utils.resolveBin = (modName, {executable = modName} = {}) => executable
     const originalArgv = process.argv
     process.argv = ['node', '../format', ...args]
     return function teardown() {
       process.argv = originalArgv
+      utils.resolveBin = originalResolveBin
     }
   }
 }

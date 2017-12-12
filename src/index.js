58f95800358b6f66b2c2190800ccdc9006105696
Corey Light <coreylight@gmail.com>
2017-12-12 11:26:03 -0600

    Use node 6 for dev

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ let shouldThrow;
 try {
   shouldThrow =
     require(`${process.cwd()}/package.json`).name === '@iopipe/scripts' &&
-    Number(process.version.slice(1).split('.')[0]) < 8;
+    Number(process.version.slice(1).split('.')[0]) < 6;
 } catch (error) {
   // ignore
 }
 
 if (shouldThrow) {
   throw new Error(
-    'You must use Node version 8 or greater to run the scripts within iopipe-scripts ' +
+    'You must use Node version 6.10 or greater to run the scripts within iopipe-scripts ' +
       'because we dogfood the untranspiled version of the scripts.'
   );
 }

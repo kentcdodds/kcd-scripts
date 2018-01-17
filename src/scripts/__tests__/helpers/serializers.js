72a7630de466e0cc4bae9ad1690b6e5b953056e4
Macklin Underdown <macklinu@users.noreply.github.com>
2018-01-17 18:43:56 -0500

    test: fix typo in serializer test helper

--- src/scripts/__tests__/helpers/serializers.js
+++ src/scripts/__tests__/helpers/serializers.js
@@ -6,7 +6,7 @@ export const unquoteSerializer = {
   test: val => typeof val === 'string',
 }
 
-// this convers windows style file paths to unix...
+// this converts windows style file paths to unix...
 export const winPathSerializer = {
   print: val => slash(val),
   test: val => typeof val === 'string' && val.includes('\\'),

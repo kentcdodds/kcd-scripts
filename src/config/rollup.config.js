716ef498fc2870f10c2536c1747bcb3a3d740723
Kent C. Dodds <kent+github@doddsfamily.us>
2018-12-11 15:33:18 -0700

    feat(rollup): simplify external
    I noticed that when I upgraded to emotion 10 in one of my packages and started using the babel plugin it swapped `@emotion/styled` for `@emotion/styled-base` which is not marked as an external with this logic and it kinda messed things up. So I improved the external to be what I think is intended.

--- src/config/rollup.config.js
+++ src/config/rollup.config.js
@@ -88,8 +88,19 @@ if (isPreact) {
 }
 
 const externalPattern = new RegExp(`^(${external.join('|')})($|/)`)
-const externalPredicate =
-  external.length === 0 ? () => false : id => externalPattern.test(id)
+
+function externalPredicate(id) {
+  const isDep = externalPattern.test(id)
+  if (umd) {
+    // for UMD, we want to bundle all non-peer deps
+    return isDep
+  }
+  // for esm/cjs we want to make all node_modules external
+  // TODO: support bundledDependencies if someone needs it ever...
+  const isNodeModule = id.includes('node_modules')
+  const isRelative = id.startsWith('.')
+  return isDep || (!isRelative && !path.isAbsolute(id)) || isNodeModule
+}
 
 const filename = [
   pkg.name,

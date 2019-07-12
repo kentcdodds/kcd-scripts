17354e6c8f7af8899d5aa0b9dea68291d52eef31
Kent C. Dodds <kent@doddsfamily.us>
2019-07-12 09:49:38 -0600

    fix(build): remove the ignored-but-copied files from the built output

--- src/scripts/build/babel.js
+++ src/scripts/build/babel.js
@@ -1,10 +1,14 @@
 const path = require('path')
 const spawn = require('cross-spawn')
+const yargsParser = require('yargs-parser')
 const rimraf = require('rimraf')
+const glob = require('glob')
 const {hasPkgProp, fromRoot, resolveBin, hasFile} = require('../../utils')
 
 const args = process.argv.slice(2)
 const here = p => path.join(__dirname, p)
+
+const parsedArgs = yargsParser(args)
 
 const useBuiltinConfig =
   !args.includes('--presets') &&
@@ -16,14 +20,15 @@ const config = useBuiltinConfig
   ? ['--presets', here('../../config/babelrc.js')]
   : []
 
-const ignore = args.includes('--ignore')
-  ? []
-  : ['--ignore', '**/__tests__/**,**/__mocks__/**']
+const builtInIgnore = '**/__tests__/**,**/__mocks__/**'
+
+const ignore = args.includes('--ignore') ? [] : ['--ignore', builtInIgnore]
 
 const copyFiles = args.includes('--no-copy-files') ? [] : ['--copy-files']
 
 const useSpecifiedOutDir = args.includes('--out-dir')
-const outDir = useSpecifiedOutDir ? [] : ['--out-dir', 'dist']
+const builtInOutDir = 'dist'
+const outDir = useSpecifiedOutDir ? [] : ['--out-dir', builtInOutDir]
 
 if (!useSpecifiedOutDir && !args.includes('--no-clean')) {
   rimraf.sync(fromRoot('dist'))
@@ -34,5 +39,18 @@ const result = spawn.sync(
   [...outDir, ...copyFiles, ...ignore, ...config, 'src'].concat(args),
   {stdio: 'inherit'},
 )
+
+// because babel will copy even ignored files, we need to remove the ignored files
+const pathToOutDir = fromRoot(parsedArgs.outDir || builtInOutDir)
+const ignoredPatterns = (parsedArgs.ignore || builtInIgnore)
+  .split(',')
+  .map(pattern => path.join(pathToOutDir, pattern))
+const ignoredFiles = ignoredPatterns.reduce(
+  (all, pattern) => [...all, ...glob.sync(pattern)],
+  [],
+)
+ignoredFiles.forEach(ignoredFile => {
+  rimraf.sync(ignoredFile)
+})
 
 process.exit(result.status)

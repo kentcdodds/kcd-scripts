de5f47a87aaabef5644afa2753851ce35b661631
Adrià Fontcuberta <afontcu@gmail.com>
2019-08-17 17:56:53 +0200

    feat(lint-staged): add support for vue files (#92)

--- src/config/lintstagedrc.js
+++ src/config/lintstagedrc.js
@@ -5,7 +5,7 @@ const doctoc = resolveBin('doctoc')
 
 module.exports = {
   'README.md': [`${doctoc} --maxlevel 3 --notitle`, 'git add'],
-  '*.+(js|jsx|json|yml|yaml|css|less|scss|ts|tsx|md|graphql|mdx)': [
+  '*.+(js|jsx|json|yml|yaml|css|less|scss|ts|tsx|md|graphql|mdx|vue)': [
     isOptedOut('autoformat', null, `${kcdScripts} format`),
     `${kcdScripts} lint`,
     `${kcdScripts} test --findRelatedTests`,

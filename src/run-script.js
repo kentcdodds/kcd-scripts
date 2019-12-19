c006b8069b9d5c64188f22c1971bab5f144ba398
Weyert de Boer <weyert@innerfuse.biz>
2019-12-19 21:50:04 +0000

    feat: Allow debugging kcd-scripts, especially when jest unit tests are running

--- src/run-script.js
+++ src/run-script.js
@@ -2,7 +2,7 @@ const path = require('path')
 const spawn = require('cross-spawn')
 const glob = require('glob')
 
-const [executor, ignoredBin, script, ...args] = process.argv
+const [executor, ignoredBin, script] = process.argv
 
 if (script) {
   spawnScript()
@@ -53,16 +53,41 @@ function getEnv() {
 }
 
 function spawnScript() {
-  const relativeScriptPath = path.join(__dirname, './scripts', script)
-  const scriptPath = attemptResolve(relativeScriptPath)
+  // get all the arguments of the script and find the position of our script commands
+  const args = process.argv.slice(2)
+  const scriptIndex = args.findIndex(
+    x =>
+      x === 'format' ||
+      x === 'lint' ||
+      x === 'pre-commit' ||
+      x === 'test' ||
+      x === 'validate' ||
+      x === 'build',
+  )
 
+  // Extract the node arguments so we can pass them to node later on
+  const buildCommand = scriptIndex === -1 ? args[0] : args[scriptIndex]
+  const nodeArgs = scriptIndex > 0 ? args.slice(0, scriptIndex) : []
+
+  if (!buildCommand) {
+    throw new Error(`Unknown script "${script}".`)
+  }
+
+  const relativeScriptPath = path.join(__dirname, './scripts', buildCommand)
+  const scriptPath = attemptResolve(relativeScriptPath)
   if (!scriptPath) {
     throw new Error(`Unknown script "${script}".`)
   }
-  const result = spawn.sync(executor, [scriptPath, ...args], {
-    stdio: 'inherit',
-    env: getEnv(),
-  })
+
+  // Attempt to strt the script with the passed node arguments
+  const result = spawn.sync(
+    executor,
+    nodeArgs.concat(scriptPath).concat(args.slice(scriptIndex + 1)),
+    {
+      stdio: 'inherit',
+      env: getEnv(),
+    },
+  )
 
   if (result.signal) {
     handleSignal(result)

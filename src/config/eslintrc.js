72b105311d818de71a6cef588887b485db62a967
Michaël De Boey <info@michaeldeboey.be>
2020-03-04 16:42:34 +0100

    Remove unnecessary require.resolve calls

--- src/config/eslintrc.js
+++ src/config/eslintrc.js
@@ -2,10 +2,10 @@ const {ifAnyDep} = require('../utils')
 
 module.exports = {
   extends: [
-    require.resolve('eslint-config-kentcdodds'),
-    require.resolve('eslint-config-kentcdodds/jest'),
-    ifAnyDep('react', require.resolve('eslint-config-kentcdodds/jsx-a11y')),
-    ifAnyDep('react', require.resolve('eslint-config-kentcdodds/react')),
+    'eslint-config-kentcdodds',
+    'eslint-config-kentcdodds/jest',
+    ifAnyDep('react', 'eslint-config-kentcdodds/jsx-a11y'),
+    ifAnyDep('react', 'eslint-config-kentcdodds/react'),
   ].filter(Boolean),
   rules: {},
 }

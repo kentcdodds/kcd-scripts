4dd19fafec4dcb146727b55837e0543a767aa659
Michaël De Boey <info@michaeldeboey.be>
2020-03-21 01:02:22 +0100

    feat(husky): expose husky config (#126)
    * Expose husky config
    
    * Resolve discussions

---
+++ src/config/huskyrc.js
@@ -0,0 +1,9 @@
+const {resolveKcdScripts} = require('../utils')
+
+const kcdScripts = resolveKcdScripts()
+
+module.exports = {
+  hooks: {
+    'pre-commit': `${kcdScripts} pre-commit`,
+  },
+}
--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   babel: require('./babelrc'),
   eslint: require('./eslintrc'),
+  husky: require('./huskyrc'),
   jest: require('./jest.config'),
   lintStaged: require('./lintstagedrc'),
   prettier: require('./prettierrc'),

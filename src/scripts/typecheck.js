efb6e0b7ede5258a81e36f6b8e5b5a464fe856c6
Kent C. Dodds <me+github@kentcdodds.com>
2020-12-02 16:03:51 -0700

    feat(TS): allow use of the typecheck file without a root-level tsconfig.json

--- src/scripts/typecheck.js
+++ src/scripts/typecheck.js
@@ -1,7 +1,9 @@
 const spawn = require('cross-spawn')
+const yargsParser = require('yargs-parser')
 const {hasAnyDep, resolveBin, hasFile} = require('../utils')
 
-const args = process.argv.slice(2)
+let args = process.argv.slice(2)
+const parsedArgs = yargsParser(args)
 
 if (!hasAnyDep('typescript')) {
   throw new Error(
@@ -9,16 +11,21 @@ if (!hasAnyDep('typescript')) {
   )
 }
 
-if (!hasFile('tsconfig.json')) {
+if (!parsedArgs.project && !hasFile('tsconfig.json')) {
   throw new Error(
-    'Cannot use the "typecheck" script in a project that does not have a tsconfig.json file.',
+    'Cannot use the "typecheck" script without --project in a project that does not have a tsconfig.json file.',
   )
 }
 
-const result = spawn.sync(
-  resolveBin('typescript', {executable: 'tsc'}),
-  ['--build', ...args],
-  {stdio: 'inherit'},
-)
+// if --project is provided, we can't pass --build
+// if --build is provided, we don't need to add it
+// if --no-build is passed, we'll just trust they know what they're doing
+if (!parsedArgs.project && !parsedArgs.build && !parsedArgs.noBuild) {
+  args = ['--build', ...args]
+}
+
+const result = spawn.sync(resolveBin('typescript', {executable: 'tsc'}), args, {
+  stdio: 'inherit',
+})
 
 process.exit(result.status)

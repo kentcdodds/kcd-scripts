ab8c8ce9c3190dd16f824fd3dbb63b8d81998929
Chris O'Donnell <dev@codfish.io>
2021-02-25 20:56:58 -0500

    fix: use built in prettier config for eslint if no local config found

--- src/config/eslintrc.js
+++ src/config/eslintrc.js
@@ -1,8 +1,17 @@
 const prettierConfig = require('./prettierrc');
+const { hasLocalConfig } = require('../utils');
+
+const args = process.argv.slice(2);
+
+const useBuiltinConfig =
+  !args.includes('--config') && !hasLocalConfig('prettier');
+const ruleValue = useBuiltinConfig ? ['error', prettierConfig] : 'error';
 
 module.exports = {
   extends: [require.resolve('eslint-config-codfish')].filter(Boolean),
   rules: {
-    'prettier/prettier': ['error', prettierConfig],
+    // Explicitly set prettier rules to use our built in config if no
+    // local prettier config is found.
+    'prettier/prettier': ruleValue,
   },
 };

6dda70c876e51cd3e0230cc37c6c23814240c50d
Michaël De Boey <info@michaeldeboey.be>
2021-04-10 00:28:24 +0200

    feat: add all file extensions from lintstagedrc as default for format script (#199)
    * feat: add all file extensions from lintstagedrc as default for format script
    
    * Update format.js.snap

--- src/scripts/format.js
+++ src/scripts/format.js
@@ -30,7 +30,7 @@ const relativeArgs = args.map(a => a.replace(`${process.cwd()}/`, ''))
 
 const filesToApply = parsedArgs._.length
   ? []
-  : ['**/*.+(js|json|less|css|ts|tsx|md)']
+  : ['**/*.+(js|jsx|json|yml|yaml|css|less|scss|ts|tsx|md|gql|graphql|mdx|vue)']
 
 const result = spawn.sync(
   resolveBin('prettier'),

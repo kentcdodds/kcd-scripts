b4bbe726041aec11954b53c4b367e6e748114f02
Michaël De Boey <info@michaeldeboey.be>
2021-04-10 00:27:34 +0200

    feat: add .gql files in lintstagedrc (#198)

--- src/config/lintstagedrc.js
+++ src/config/lintstagedrc.js
@@ -5,7 +5,7 @@ const doctoc = resolveBin('doctoc')
 
 module.exports = {
   'README.md': [`${doctoc} --maxlevel 3 --notitle`],
-  '*.+(js|jsx|json|yml|yaml|css|less|scss|ts|tsx|md|graphql|mdx|vue)': [
+  '*.+(js|jsx|json|yml|yaml|css|less|scss|ts|tsx|md|gql|graphql|mdx|vue)': [
     `${kcdScripts} format`,
     `${kcdScripts} lint`,
     `${kcdScripts} test --findRelatedTests`,

e3e73a0e68220dcdbd8a3e4bdf4b5e68bc88fe42
Michaël De Boey <info@michaeldeboey.be>
2021-04-25 16:08:30 +0200

    chore: use jest-snapshot-serializer-raw/always as unquoteSerializer

--- src/scripts/__tests__/lint.js
+++ src/scripts/__tests__/lint.js
@@ -1,9 +1,7 @@
 import cases from 'jest-in-case'
-import {
-  unquoteSerializer,
-  winPathSerializer,
-  relativePathSerializer,
-} from './helpers/serializers'
+import * as unquoteSerializer from 'jest-snapshot-serializer-raw/always'
+
+import {winPathSerializer, relativePathSerializer} from './helpers/serializers'
 
 expect.addSnapshotSerializer(unquoteSerializer)
 expect.addSnapshotSerializer(winPathSerializer)
--- src/scripts/__tests__/validate.js
+++ src/scripts/__tests__/validate.js
@@ -1,5 +1,5 @@
 import cases from 'jest-in-case'
-import {unquoteSerializer} from './helpers/serializers'
+import * as unquoteSerializer from 'jest-snapshot-serializer-raw/always'
 
 expect.addSnapshotSerializer(unquoteSerializer)
 

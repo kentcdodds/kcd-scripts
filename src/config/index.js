863c25a21d8fc18122d5fe51233fe280a1759b2d
Chris O'Donnell <dev@codfish.io>
2021-08-18 14:41:58 -0400

    feat: migrate to husky v7
    BREAKING CHANGE: husky config has been totally removed and
    huskys v4 setup no longer works. You will need to migrate to
    v7. More info: https://github.com/typicode/husky-4-to-7
    
    **TL;DR**
    
    Run the following:
    ```
    npm exec -- github:typicode/husky-4-to-7 --remove-v4-config
    ```
    
    You will need to edit `.husky/commit-msg` after running the above command.
    Change `-E HUSKY_GIT_PARAMS` --> `--edit $1`.

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,6 @@ module.exports = {
   babel: require('./babelrc'),
   commitlint: require('./commitlint.config'),
   eslint: require('./eslintrc'),
-  husky: require('./huskyrc'),
   jest: require('./jest.config'),
   lintStaged: require('./lintstagedrc'),
   prettier: require('./prettierrc'),

kentcdodds/kcd-scripts

commit 438ed612db87462b5424a748b2d546c57d7bfdac

fix: use `modulePaths` to improve Jest 27 imports (#220)

src/config/jest.config.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const {ifAnyDep, hasFile, hasPkgProp, fromRoot} = require('../utils')
+const {ifAnyDep, hasFile, hasPkgProp} = require('../utils')
 
 const here = p => path.join(__dirname, p)
 
@@ -15,7 +15,7 @@ const ignores = [
 ]
 
 const jestConfig = {
-  roots: [fromRoot('src')],
+  roots: ['<rootDir>/src'],
   testEnvironment: ifAnyDep(
     ['webpack', 'rollup', 'react', 'preact'],
     'jsdom',
@@ -23,12 +23,7 @@ const jestConfig = {
   ),
   testURL: 'http://localhost',
   moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx'],
-  moduleDirectories: [
-    'node_modules',
-    fromRoot('src'),
-    'shared',
-    fromRoot('tests'),
-  ],
+  modulePaths: ['<rootDir>/src', 'shared', '<rootDir>/tests'],
   collectCoverageFrom: ['src/**/*.+(js|jsx|ts|tsx)'],
   testMatch: ['**/__tests__/**/*.+(js|jsx|ts|tsx)'],
   testPathIgnorePatterns: [...ignores],
@@ -59,7 +54,7 @@ const setupFiles = [
 ]
 for (const setupFile of setupFiles) {
   if (hasFile(setupFile)) {
-    jestConfig.setupFilesAfterEnv = [fromRoot(setupFile)]
+    jestConfig.setupFilesAfterEnv = `<rootDir>/${setupFile}`
   }
 }
 

commit dc1bd7b16306e5fbedf7824834cc07bc2c2c990f

Fix `setupFilesAfterEnv`: must be an array
Fixes a regression from #220

src/config/jest.config.js
@@ -54,7 +54,7 @@ const setupFiles = [
 ]
 for (const setupFile of setupFiles) {
   if (hasFile(setupFile)) {
-    jestConfig.setupFilesAfterEnv = `<rootDir>/${setupFile}`
+    jestConfig.setupFilesAfterEnv = [`<rootDir>/${setupFile}`]
   }
 }
 